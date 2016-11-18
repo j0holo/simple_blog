93854eb07b030518c56f02dbf29f194110a5f0ec
Ruben Klink <rubenjklink@gmail.com>
2016-11-18 20:42:59 +0100

    Did a litle bit of boyscouting

--- app/static/js/shortcuts.js
+++ app/static/js/shortcuts.js
@@ -3,17 +3,18 @@
  * writing or updating a post.
  */
 function insert_four_spaces(e) {
-    if (e.ctrlKey && (e.keyCode || e.which == 32)) {
+    if (e.ctrlKey && (e.keyCode == 32)) {
+        e.preventDefault();
         var textarea = document.getElementById("textarea");
-        var four_spaces = "    "; // this is stupid and error prone
+        var spaces = "    "; // this is stupid and error prone
         var cursorPos = textarea.selectionStart;
         var textBeforeCursor = textarea.value.substring(0, cursorPos);
         var textAfterCursor = textarea.value.substring(
             textarea.selectionEnd,
             textarea.value.length
         );
-        textarea.value = textBeforeCursor + four_spaces + textAfterCursor;
-        cursorPos = cursorPos + four_spaces.length;
+        textarea.value = textBeforeCursor + spaces + textAfterCursor;
+        cursorPos = cursorPos + spaces.length;
         textarea.selectionStart = cursorPos;
         textarea.selectionEnd = cursorPos;
         textarea.focus();
